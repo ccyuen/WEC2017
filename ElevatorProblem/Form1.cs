de769b7117ec53ab928bddc02145150189abb0a2
Clement Yuen <ccyuen@edu.uwaterloo.ca>
2017-05-12 23:55:31 -0400

    Completed system timer and added test cases

--- ElevatorProblem/Form1.cs
+++ ElevatorProblem/Form1.cs
@@ -9,17 +9,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Timers;
 
 namespace ElevatorProblem
 {
     public partial class Form1 : Form
     {
+        private static int i = 0;
         public Form1()
         {
-            String path = Path.GetFullPath(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\elevator_practice1.json"));
+            //String path = Path.GetFullPath(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\elevator_practice1.json"));
             //Debug.WriteLine("Path: " + path + '\n');
-            FileIO.readFromJsonFile(path);
+            //FileIO.readFromJsonFile(path);
             //Utils.printEvents(FileIO._events);
+            
+            // start the system clock once the program runs
+            System.Timers.Timer time = new System.Timers.Timer();
+            time.Interval = 1000;
+            time.Enabled = true;
+            time.AutoReset = true;
+            time.Elapsed += new ElapsedEventHandler(counter);
+
+        }
+
+        public static void counter(object source, ElapsedEventArgs e)
+        {
+            i++;
+            Debug.WriteLine("Counter: " + i);
         }
     }
 }
---
+++ ElevatorProblem/Time.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorProblem
+{
+    class Time
+    {
+        private static DateTime time = DateTime.Now;
+        
+        public static void getTime()
+        {
+            Debug.WriteLine(time.ToString());
+        }
+    }
+}
